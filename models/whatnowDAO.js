d37d67fe9754df0453194fdb3d5285f60000b47c
Gonçalo Martins <45426228+icantplay@users.noreply.github.com>
2020-06-10 17:52:31 +0100

    Add files via upload

---
+++ models/whatnowDAO.js
@@ -0,0 +1,165 @@
+
+var db = require('./conn').pool;
+
+
+
+
+
+module.exports.getEventInfo = function(callback, next) {
+    db.getConnection(function(err, conn) {
+      if (err) {
+        conn.release();
+        next(err);
+      } else
+        conn.query("Select Name, DateStart, DateEnd, Description, Street, PostalCode, Town From event", function(err, rows) {
+         
+          callback(false,{ code: 200, status: "Ok", data:rows});
+          conn.release();
+          
+        });
+    });
+  };
+  module.exports.getEventfullInfo = function(id,callback, next) {
+    db.getConnection(function(err, conn) {
+      if (err) {
+        conn.release();
+        next(err);
+      } else
+        conn.query("Select Description From event Where id = " +id, function(err, rows) {
+          
+          callback(false,{code: 200, status:"ok",data:rows});
+          conn.release();
+          
+        });
+    });
+  };
+  module.exports.getEventbyContact = function(id,callback, next) {
+    db.getConnection(function(err, conn) {
+      if (err) {
+        conn.release();
+        next(err);
+      } else
+        conn.query("Select Name, DateStart, DateEnd, Description, Street, PostalCode, Town From event Where ApplicationUserId = " +id, function(err, rows) {
+         
+          callback(false,{ code: 200, status: "Ok", data:rows});
+          conn.release();
+          
+        });
+    });
+  };
+
+  module.exports.postEventRating= function(data,callback, next) {
+    
+    db.getConnection(function(err, conn) {
+      if (err) {
+        conn.release();
+        next(err);
+      } else
+        conn.query("INSERT INTO `applicationusereventrating`(`Rating`, `EventId`) Values(?,?)",[data.rating,data.eventId], function(err, rows) {
+          
+          conn.release();
+                if (err) {
+                    console.log(err);
+                    callback(err, { code: 500, status: "Error in a database query" });
+                    return;
+                }
+                callback({ code: 200, status: "ok"});
+            })
+        
+    })
+  };  
+ 
+
+
+
+
+ //New Dao USER
+ module.exports.getAdmin = function(callback, next) {
+  db.getConnection(function(err, conn) {
+    if (err) {
+      conn.release();
+      next(err);
+    } else
+      conn.query("Select * From applicationuser Where id =5", function(err, rows) {
+        
+        callback(false,{ code: 200, status: "Ok", data:rows});
+        conn.release();
+        
+      });
+  });
+};
+
+  module.exports.getUsername = function(callback, next) {
+    db.getConnection(function(err, conn) {
+      if (err) {
+        conn.release();
+        next(err);
+      } else
+        conn.query("Select Name,id From applicationuser", function(err, rows) {
+          
+          callback(false,{ code: 200, status: "Ok", data:rows});
+          conn.release();
+          
+        });
+    });
+  };
+
+  module.exports.postLog = function(data,callback, next) {
+    console.log(data);
+    db.getConnection(function(err, conn) {
+      if (err) {
+        conn.release();
+        next(err);
+      } else
+        conn.query("Insert into loghistory(Timestamp,ApplicationUserId,LogTypeId,Coordinates) Values(CURDATE(),?,4,Point(?,?))",[data.id,data.coordinates.x,data.coordinates.y], function(err, rows) {
+          
+          conn.release();
+                if (err) {
+                    console.log(err);
+                    callback(err, { code: 500, status: "Error in a database query" });
+                    return;
+                }
+                callback({ code: 200, status: "ok"});
+            })
+        
+    })
+  };  
+ 
+
+
+ //new Dao SPOT
+  module.exports.getSpotInfo = function(callback, next) {
+    db.getConnection(function(err, conn) {
+      if (err) {
+        conn.release();
+        next(err);
+      } else
+        conn.query("Select Name,Contact,Description,Street,PostalCode,Town,Country From spot", function(err, rows) {
+          
+          callback(false,{ code: 200, status: "Ok", data:rows});
+          conn.release();
+          
+        });
+    });
+  };
+  
+
+  
+  module.exports.getSpotbyContact = function(id,callback, next) {
+    db.getConnection(function(err, conn) {
+      if (err) {
+        conn.release();
+        next(err);
+      } else
+        conn.query("Select Name,Contact,Description,Street,PostalCode,Town,Country From spot Where ApplicationUserId = " +id, function(err, rows) {
+         
+          callback(false,{ code: 200, status: "Ok", data:rows});
+          conn.release();
+          
+        });
+    });
+  };
+
+
+
+
